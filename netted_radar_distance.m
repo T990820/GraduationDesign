bf357ac0ef03238994f81058734797be692f0b0a
TianNongkai <forthefuture@hrbeu.edu.cn>
2021-03-06 21:47:52 +0800

    实现了双雷达与三雷达组网的作用距离仿真

--- netted_radar_distance.m
+++ netted_radar_distance.m
@@ -1,4 +1,35 @@
 %%
+%2部雷达不组网的探测范围
+clc;
+r=30;%单部雷达探测距离30km
+theta=0:pi/1000:2*pi;
+x=-30+r*cos(theta);
+y=r*sin(theta);
+plot(30,0,'rx',x,y);hold on;plot(30,0,'ro',x,y);hold on;
+plot(-30,0,'rx',x,y);hold on;plot(-30,0,'ro',x,y);hold on;
+plot(x,y,'b');hold on;
+x=30+r*cos(theta);
+y=r*sin(theta);
+plot(x,y,'b');hold on;
+axis([-80 80 -80 80]);
+grid on;
+%%
+%2部雷达组网的探测范围
+clc;
+for x=-80:1:80
+    for y=-80:1:80
+        R1=sqrt((x+30)*(x+30)+y*y);
+        R2=sqrt((x-30)*(x-30)+y*y);
+        SNR=810000/(R1*R1*R1*R1)+810000/(R2*R2*R1*R1)+810000/(R2*R2*R2*R2);
+         if(SNR>1)
+            plot(x,y,'ro');
+            hold on;
+            axis([-100 100 -100 100]);
+            grid on;
+        end
+    end
+end
+%%
 %3部雷达不组网的探测范围
 clc;
 r=30;%单部雷达探测距离30km
@@ -9,30 +40,30 @@ figure(1);
 plot(0,0,'rx',x,y);hold on;plot(0,0,'ro',x,y);hold on;
 plot(30,30,'rx',x,y);hold on;plot(30,30,'ro',x,y);hold on;
 plot(-30,30,'rx',x,y);hold on;plot(-30,30,'ro',x,y);hold on;
-plot(x,y,'r');hold on;
+plot(x,y,'b');hold on;
 x=r*cos(theta);
 y=r*sin(theta);
-plot(x,y,'r');hold on;
+plot(x,y,'b');hold on;
 x=-30+r*cos(theta);
 y=30+r*sin(theta);
-plot(x,y,'r');hold on;
+plot(x,y,'b');hold on;
 grid on;
 axis([-80 80 -80 80]);
 xlabel("距离/km");ylabel("距离/km");title("3部雷达不组网的探测范围示意图");
 %%
-%3部组网雷达的探测范围
+%3部雷达组网的探测范围
 clc;
-for x=-80:0.1:80
-    for y=-80:0.1:80
-        R=sqrt(x*x+y*y);
-        Rr=R;
-        Rt1=sqrt((x+30)*(x+30)+(y-30)*(y-30));
-        Rt2=sqrt((x-30)*(x-30)+(y+30)*(y+30));
-        SNRmonostatic=810000/(R^4);
-        SNRnetted=SNRmonostatic*R*R*R*R*(1/Rt1/Rr+1/Rt2/Rr);
+for x=-80:2:80
+    for y=-80:2:80
+        R1=sqrt((x+30)*(x+30)+(y-30)*(y-30));
+        R2=sqrt((x-30)*(x-30)+(y-30)*(y-30));
+        R3=sqrt(x*x+y*y);
+        SNRnetted=810000/(R1*R1*R1*R1)+810000/(R2*R2*R2*R2)+810000/(R3*R3*R3*R3)+810000/(R1*R1*R3*R3)+810000/(R2*R2*R3*R3)+810000/(R1*R1*R2*R2);
         if(SNRnetted>1)
-            plot(x,y,'o');
+            plot(x,y,'ro');
+            hold on;
+            axis([-100 100 -100 100]);
+            grid on;
         end
     end
 end
-grid on;
