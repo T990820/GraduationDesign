597c24a7104838dbb410a99335687d64052f429a
TianNongkai <forthefuture@hrbeu.edu.cn>
2021-03-06 09:56:13 +0800

    增加组网雷达探测距离分析

--- netted_radar_distance.m
+++ netted_radar_distance.m
@@ -5,6 +5,7 @@ r=30;%µ¥²¿À×´ïÌ½²â¾àÀë30km
 theta=0:pi/1000:2*pi;
 x=30+r*cos(theta);
 y=30+r*sin(theta);
+figure(1);
 plot(0,0,'rx',x,y);hold on;plot(0,0,'ro',x,y);hold on;
 plot(30,30,'rx',x,y);hold on;plot(30,30,'ro',x,y);hold on;
 plot(-30,30,'rx',x,y);hold on;plot(-30,30,'ro',x,y);hold on;
@@ -20,3 +21,18 @@ axis([-80 80 -80 80]);
 xlabel("¾àÀë/km");ylabel("¾àÀë/km");title("3²¿À×´ï²»×éÍøµÄÌ½²â·¶Î§Ê¾ÒâÍ¼");
 %%
 %3²¿×éÍøÀ×´ïµÄÌ½²â·¶Î§
+clc;
+for x=-80:0.1:80
+    for y=-80:0.1:80
+        R=sqrt(x*x+y*y);
+        Rr=R;
+        Rt1=sqrt((x+30)*(x+30)+(y-30)*(y-30));
+        Rt2=sqrt((x-30)*(x-30)+(y+30)*(y+30));
+        SNRmonostatic=810000/(R^4);
+        SNRnetted=SNRmonostatic*R*R*R*R*(1/Rt1/Rr+1/Rt2/Rr);
+        if(SNRnetted>1)
+            plot(x,y,'o');
+        end
+    end
+end
+grid on;
